ed70f816d1512d1991ce83b4fe667e0b9206372a
RishikaGoyal <rishikagoyal175@gmail.com>
2023-06-01 14:48:35 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hypen_dot='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list of requirements
+    
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ") for req in requirements]
+
+        if Hypen_dot in requirements:
+            requirements.remove(Hypen_dot)
+
+    return requirements
+
+
+setup(
+
+name='ml_project',
+version='0.0.1',
+author='rishika',
+author_email='rishikagoyal51@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+
+
+)
